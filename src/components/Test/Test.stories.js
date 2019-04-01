46980f62bfbb57de9de1fc40c97cc7892b0cf45a
Gabriele Venturi <gabriele@Gabrieles-MacBook-Pro.local>
2019-04-01 13:52:12 +0200

    Updated gitignore

--- src/components/Test/Test.stories.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-
-import Test from "./Test";
-
-storiesOf("Test", module).add("default", () => <Test />);
